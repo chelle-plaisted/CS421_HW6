9b7f6f02b0486b6a68e408a4060f937bbe80c455
chelle-plaisted <plaisted20@up.edu>
2018-12-02 23:05:49 -0800

    updates after workin g with brandon

--- AI/Effie.py
+++ AI/Effie.py
@@ -7,9 +7,9 @@ from Constants import *
 from Construction import CONSTR_STATS
 from Ant import UNIT_STATS
 from Move import Move
-from GameState import addCoords
+from GameState import *
 from AIPlayerUtils import *
-
+import os
 
 ##
 #AIPlayer
@@ -36,10 +36,14 @@ class AIPlayer(Player):
         self.myFood = None
         self.myTunnel = None
         self.myHill = None
-        self.stateUtil = self.initializeStateUtil() # {state: utility} # TODO check file structure and initialize
+        self.file = './delplanc18_plaisted20_state_util.txt'
+        self.stateUtil = self.initializeStateUtil() # {state: utility} 
         self.discountFactor = 0.95 # Constant
         self.learningRate = 1.0 # will exponentially decline
         self.explorationRate = 1.0 # will exponentially decline
+        self.tickingClock = 0 # will increase after every game
+        self.reward = 0 # no rewards yet
+        self.discount = 0.95 #static variable
         self.lastState = None
 
 
@@ -91,26 +95,48 @@ class AIPlayer(Player):
         if (self.myHill == None):
             self.myHill = getConstrList(currentState, currentState.whoseTurn, (ANTHILL,))[0]
         if (self.myFood == None):
-            foods = getCurrPlayerFood(self, currentState)
+            self.myFood = getCurrPlayerFood(self, currentState)
+        
+        self.explorationRate = 1-self.tickingClock**2/1000000
+        self.learningRate = 1-self.tickingClock**2/1000
+       
+        self.reward += -0.001 # this will give a small penalty for every move made to encourage it to win faster
+        consolidatedCurrent = self.consolidateState(currentState)
+        if self.lastState is None:
+            consolidatedLast = ""
+        else:
+            consolidatedLast = self.consolidateState(self.lastState)
+        
+        if consolidatedLast in self.stateUtil:
+            lastStateUtil = self.stateUtil[consolidatedLast]
+        else:
+            lastStateUtil = 0
+        if consolidatedCurrent in self.stateUtil:
+            currentUtil = self.stateUtil[consolidatedCurrent]
+        else:
+            currentUtil = 0
+        self.stateUtil[consolidatedLast] = lastStateUtil + self.learningRate*(self.reward+self.discount*currentUtil-lastStateUtil)
+
 
         # save this state in case the game ends
         self.lastState = currentState
-        # update stateUtil for self.lastState by applying TD learning equation
         # make a move
         moveType = random.random() # [0, 1]
-
-        if moveType > self.explorationRate:
-            return self.makeExploitationMove(currentState)
-        else:
+        move = None
+        if moveType > self.explorationRate: 
+            move = self.makeExploitationMove(currentState)
+        if move is None:
             moves = listAllLegalMoves(currentState)
-            selectedMove = moves[random.randint(0,len(moves) - 1)];
+            selectedMove = moves[random.randint(0,len(moves) - 1)]
 
             #don't do a build move if there are already 3+ ants
             numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
             while (selectedMove.moveType == BUILD and numAnts >= 3):
-                selectedMove = moves[random.randint(0,len(moves) - 1)];
+                selectedMove = moves[random.randint(0,len(moves) - 1)]
 
             return selectedMove
+
+         
 
     ##
     #getAttack
@@ -126,20 +152,34 @@ class AIPlayer(Player):
     # save the stateUtil mapping to a file and updates the learning and exploration rates
     #
     def registerWin(self, hasWon):
-        reward = hasWon
+        if hasWon:
+            reward = 1
+        else: 
+            reward = -1
         # update stateUtil for self.lastState by applying TD learning equation
-        # update learning rate
-        # update exploration rate
+            # get consolidated state
+        s = self.consolidateState(self.lastState)
+        if s in self.stateUtil:
+            old = self.stateUtil[s]
+            newUtil = old + self.learningRate * self.reward
+            self.stateUtil[s] = newUtil
+        else:
+            self.stateUtil[s] = self.learningRate * self.reward
+        self.tickingClock += 1 # increment tickingClock
+        self.reward = 0 # reset reward
         self.saveUtils()
 
     ######################### NEW TD LEARNING METHODS ##########################
-    ## TODO complete
+    ##
     # initializeStateUtil
     # Description: Sets the stateUtil dict as the contents of
     # delplanc18_plaisted20_state_util.txt or empty.
     #
     def initializeStateUtil(self):
-        return {}
+        if os.path.exists(self.file):
+            return self.loadUtils()
+        else:
+            return {}
 
     ##
     # consolidateState
@@ -147,80 +187,225 @@ class AIPlayer(Player):
     #
     # Parameters:
     #   state: GameState to consolidate.
+    # Return: a string representing a consolidated state
     #
     def consolidateState(self, state):
         # helper variables
-        queen = myInv.getQueen()
+        # print("This is our state: ", state)
+        queen = getAntList(state, state.whoseTurn, (QUEEN,))[0]
         workers = getAntList(state, state.whoseTurn, (WORKER,))
 
-        simpleState = []
+        simpleState = ''
 
         # number of workers
-        simpleState.append(len(workers))
+        simpleState += 'Effie has ' + str(len(workers)) + ' worker(s).'
         # amount of food
-        simpleState.append(state.inventories[state.whoseTurn].foodCount)
+        simpleState += ' Effie has ' + str(state.inventories[state.whoseTurn].foodCount)
+        simpleState += ' item(s) of food in inventory.'
         # average dist of carrying ants/non=carrying ants
         numCarry = 0
         sumCarry = 0
         numForage = 0
         sumForage = 0
-        for worker in myWorkers:
+        for worker in workers:
             if worker.carrying:
                 numCarry += 1
                 sumCarry += (min(approxDist(worker.coords, self.myHill.coords),
                                approxDist(worker.coords, self.myTunnel.coords)))
-                # # Encourage worker to go on hill/tunnel if it has food
-                # if min(approxDist(worker.coords, myHill.coords),
-                # approxDist(worker.coords, myTunnel.coords)) == 0:
-                #     sumCarry += 20
             else:
                 numForage += 1
-                sumForage += (min(approxDist(worker.coords, self.myFoods[0].coords),
-                               approxDist(worker.coords, self.myFoods[1].coords)))
-                # # Encourage worker to go on food if it does not have food
-                # if min(approxDist(worker.coords, myFoods[0].coords),
-                # approxDist(worker.coords, myFoods[1].coords)) == 0:
-                #     sumForage += 20
+                sumForage += (min(approxDist(worker.coords, self.myFood[0].coords),
+                               approxDist(worker.coords, self.myFood[1].coords)))
         if numCarry > 0:
-            simpleState.append(sumCarry / float(numCarry))
+            carryVal = sumCarry / float(numCarry)
         else:
-            simpleState.append(0)
-
+            carryVal = 0
         if numForage > 0:
-            simpleState.append(sumForage / float(numForage))
+            forageVal = sumForage / float(numForage)
         else:
-            simpleState.append(0)
+            forageVal = 0
+        simpleState += ' The average distance of a carrying worker from its target is ' + str(carryVal)
+        simpleState += ' steps. The average distance of a non-carrying worker from its target is ' + str(forageVal)
+        simpleState += ' steps.'
 
         # queen is on anthill
         if queen.coords == self.myHill.coords:
-            simpleState.append(1)
+            simpleState += ' The queen is on the anthill.'
         else:
-            simpleState.append(0)
-        # queen is on tunnell
-        if queen.coords == self.myTunnel.coords:
-            simpleState.append(1)
+            simpleState += ' The queen is not on the anthill.'
+        # queen is on tunnel
+        if queen.coords is self.myTunnel.coords:
+            simpleState += ' The queen is on the tunnel.'
         else:
-            simpleState.append(0)
+            simpleState += ' The queen is not on the tunnel.'
+
+        return simpleState.strip()
+
     ##
     # saveUtils
     # Description: writes the current stateUtil dictionary to a file.
     # File Name delplanc18_plaisted20_state_util.txt
     #
     def saveUtils(self):
-        pass
+        with open(self.file, "w") as file:
+              for key, value in self.stateUtil.items():
+                file.write("" + str(key) + "|" + str(value))
+                file.write("\n")
 
     ##
     # loadUtils
     # Description: reads the values from file name delplanc18_plaisted20_state_util.txt
     #
     def loadUtils(self):
-        pass
+        data = {}
+        with open(self.file) as file:
+          for line in file:
+            line = line.strip()
+            (key,val) = line.split("|")
+            data[key] = float(val)
 
-    ##
+        return data
+
     # makeExploitationMove
     # Description: pick the move that leads to the highest utility state. Ignore
     # all moves without corresponding utility values. Break ties randomly and choose
     # random move if no states have utilities
     #
     def makeExploitationMove(self, state):
-        pass
+        legalMoves = listAllLegalMoves(state)
+        #stateArray = []
+        utilities = {}
+        for move in legalMoves:
+            state = getNextState(state,move)
+            consolidatedState = self.consolidateState(state)
+            #stateArray.append(consolidatedState)
+            if consolidatedState in self.stateUtil:
+                utilities[move] = self.stateUtil[consolidatedState]
+
+        # establishes the highest utilites that were found and the best state
+        highestUtility = -100000
+        bestMove = None
+
+        # checks the list of utilites for comparisons
+        for key in utilities.keys():
+            if utilities[key] > highestUtility: #if new utility is higher, it replaces the current highest
+                highestUtility = utilities[key]
+                bestMove = key
+            elif utilities[key] == highestUtility: #if the utilites are tied, there is a random tiebreaker
+                score1 = random.random()
+                score2 = random.random()
+                if score1 > score2:
+                    bestMove = key
+        return bestMove
+            
+        
+
+
+    ## FROM AIPLAYERUTILS for testing
+    # Return: a list of the food objects on my side of the board
+    def getCurrPlayerFood(self, currentState):
+        food = getConstrList(currentState, 2, (FOOD,))
+        myFood = []
+        if (currentState.inventories[0].player == currentState.whoseTurn):
+            myFood.append(food[2])
+            myFood.append(food[3])
+        else:
+            myFood.append(food[0])
+            myFood.append(food[1])
+        return myFood
+
+################################################################################
+##############################TEST CASES########################################
+################################################################################
+testAgent = AIPlayer(PLAYER_ONE)
+
+################################################################################
+# consolidateState
+######################################################################
+testState = GameState.getBasicState()
+food1 = Building((5, 0), FOOD, NEUTRAL)
+food2 = Building((4, 0), FOOD, NEUTRAL)
+food3 = Building((4, 9), FOOD, NEUTRAL)
+food4 = Building((5, 9), FOOD, NEUTRAL)
+hill = Building((0, 0), ANTHILL, PLAYER_ONE)
+tunnel = Building((5,1), TUNNEL, PLAYER_ONE)
+testState.inventories[NEUTRAL].constrs += [food1, food2]
+testState.inventories[NEUTRAL].constrs += [food3, food4]
+p1Worker = Ant((5, 4), WORKER, PLAYER_ONE)
+queen = Ant((0, 0), QUEEN, PLAYER_ONE)
+testState.inventories[0].ants.append(p1Worker)
+testState.inventories[0].ants.append(queen)
+testState.inventories[0].foodCount = 10
+testState.whoseTurn = PLAYER_ONE
+testAgent.myFood = testAgent.getCurrPlayerFood(testState)
+testAgent.myTunnel = getConstrList(testState, testState.whoseTurn, (TUNNEL,))[0]
+testAgent.myHill = getConstrList(testState, testState.whoseTurn, (ANTHILL,))[0]
+
+s1 = testAgent.consolidateState(testState)
+goalState1 = """
+ Effie has 1 worker(s). Effie has 10 item(s) of food in inventory. The average distance of a carrying worker from its target is 0 steps. The average distance of a non-carrying worker from its target is 5.0 steps. The queen is on the anthill. The queen is not on the tunnel.
+"""
+if not s1.strip() == goalState1.strip():
+    print('Effie failed consolidateState test 1. Expected:\n', goalState1, '\nRecieved:\n', s1, '\n')
+
+queen.coords = (9,9)
+p1Worker2 = Ant((8, 9), WORKER, PLAYER_ONE)
+p1Worker3 = Ant((0, 1), WORKER, PLAYER_ONE)
+p1Worker3.carrying = True
+
+testState.inventories[0].ants.append(p1Worker2)
+testState.inventories[0].ants.append(p1Worker3)
+
+s2 = testAgent.consolidateState(testState)
+goalState2 = """
+ Effie has 3 worker(s). Effie has 10 item(s) of food in inventory. The average distance of a carrying worker from its target is 1.0 steps. The average distance of a non-carrying worker from its target is 4.0 steps. The queen is on the anthill. The queen is not on the tunnel.
+"""
+if not s2.strip() == goalState2.strip():
+    print('Effie failed consolidateState test 2. Expected:\n', goalState2, '\nRecieved:\n', s2, '\n')
+
+################################################################################
+# saveUtils
+######################################################################
+testAgent.stateUtil = {'I am a consolidated state.' : 0.0,
+    'I am also a consolidated state' : 1.0,
+    'I am the best of the consolidated states.': 2.0}
+testAgent.file = 'test.txt'
+testAgent.saveUtils()
+
+with open(testAgent.file, "r") as f:
+    data = f.read()
+
+goalContent = """
+I am a consolidated state.|0.0
+I am also a consolidated state|1.0
+I am the best of the consolidated states.|2.0
+"""
+
+if not data.strip() == goalContent.strip():
+    print('Effie failed saveUtils test 1. Expected:\n', goalContent, '\nRecieved:\n', data, '\n')
+
+
+################################################################################
+# loadUtils
+######################################################################
+old = testAgent.stateUtil
+new = testAgent.loadUtils()
+
+if not old == new:
+    print('Effie failed loadUtils test 1. Expected:\n', old, '\nRecieved:\n', new, '\n')
+
+################################################################################
+# initializeStateUtil
+################################################################################
+testAgent.stateUtil = None
+testAgent.stateUtil = testAgent.initializeStateUtil()
+if not old == testAgent.stateUtil:
+    print('Effie failed initializeStateUtil test 1. Expected:\n', old, '\nRecieved:\n', testAgent.stateUtil, '\n')
+
+os.remove(testAgent.file)
+testAgent.stateUtil = testAgent.initializeStateUtil()
+if not testAgent.stateUtil == {}:
+    print('Effie failed initializeStateUtil test 2. Expected:\nNone \nRecieved:\n', testAgent.stateUtil, '\n')
+################################################################################
+# makeExploitationMove
+######################################################################
